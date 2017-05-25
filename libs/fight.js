0c9b57994d16626e42b69c1c16ca793cfcc90672
Sergey Timchenko <art-street93@mail.ru>
2017-05-25 22:30:04 +0300

    Fixes

--- libs/fight.js
+++ libs/fight.js
@@ -1,21 +1,29 @@
 import isDead from './isDead';
 
 const fight = (fighter, improvedFighter, ...point) => {
-    let i = 0;
+    let i         = 0;
+    let noOneDied = true;
+    
     for (let item of point) {
         if (i % 2 === 0) {
             fighter.hit(improvedFighter, item);
             if (isDead(improvedFighter)) {
+                noOneDied = false;
                 break;
             }
         }
         else {
             improvedFighter.hit(fighter, item);
             if (isDead(fighter)) {
+                noOneDied = false;
                 break;
             }
         }
         i++;
+    }
+
+    if (noOneDied) {
+        console.log(`No one died. Everyone is alive.`);
     }
 };
 
